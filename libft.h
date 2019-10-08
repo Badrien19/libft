d014fe4856e64e7d85d557cc4351652d43fd49bb
Baar Adrien <badrien@w-r3-p7.s19.be>
2019-10-08 16:10:37 +0200

    add fT_atoi + change libft and makefile to work with ft_atoi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: badrien <badrien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 11:07:16 by badrien           #+#    #+#             */
-/*   Updated: 2019/10/08 14:42:06 by badrien          ###   ########.fr       */
+/*   Updated: 2019/10/08 16:00:57 by badrien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *phrase, const char *mot, int len)
+char	*ft_strnstr(const char *phrase, const char *mot, int len);
+int	ft_atoi(const char *str);
 
 #endif

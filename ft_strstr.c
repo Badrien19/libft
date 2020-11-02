16920dc133178a5d02088a8ed8f5bce8a7743b15
Badrien19 <adrienbaar@hotmail.com>
2020-11-02 16:56:45 +0100

    add strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 16:55:36 by user42            #+#    #+#             */
+/*   Updated: 2020/11/02 16:55:44 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char    *ft_strstr(const char *haystack, const char *needle)
+{
+    size_t    i;
+    size_t    j;
+    char    *first_occ;
+
+    i = 0;
+    j = 0;
+    if (ft_strlen(needle) == 0)
+        return (char*)(&haystack[0]);
+    while (haystack[j])
+    {
+        if (needle[0] == haystack[j])
+            first_occ = (char*)(&haystack[j]);
+        while (needle[i] == haystack[j + i])
+        {
+            i++;
+            if (i == ft_strlen(needle))
+                return (first_occ);
+        }
+        i = 0;
+        j++;
+    }
+    return (NULL);
+}
